32e28c4cc99e9c5ef0ebf741d269bc037948ec88
aepContainer <eray.palazz@gmail.com>
2024-12-19 14:58:02 +0300

    basit yapı geliştirildi

--- src/main.c
+++ src/main.c
@@ -1,1 +1,13 @@
-#include ""
+#include "../inc/cub3d.h"
+
+int main(int argc, char **argv)
+{
+	t_cube	*cube;
+
+	if (argc != 2)
+		return (EXIT_FAILURE);// error message
+	cube = parser(argv);// error messages
+	if (!cube)
+		return (EXIT_FAILURE);// no error message!
+	executer(cube);
+}

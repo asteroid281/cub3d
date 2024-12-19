182896b22ad6fde3e167c0644b87f4ef5a7b8292
aepContainer <eray.palazz@gmail.com>
2024-12-19 16:28:57 +0300

    executer basit baslangic yapildi

--- src/main.c
+++ src/main.c
@@ -2,11 +2,12 @@
 
 int main(int argc, char **argv)
 {
-	t_cube	*cube;
+	t_cube	cube;
 
+	cube = (t_cube){0};
 	if (argc != 2)
 		return (EXIT_FAILURE);// error message
-	cube = parser(argv);// error messages
+	parser(argv, cube);// error messages
 	if (!cube)
 		return (EXIT_FAILURE);// no error message!
 	executer(cube);

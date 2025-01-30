392b9cd3307e887cdd63ea177cfaec578f75c5dd
Ali Eren Palaz <apalaz@k1m26s02.42istanbul.com.tr>
2025-01-30 19:46:04 +0300

    src'de ufak gelistirmeler yapildi

--- src/executer/set_all_data_to_window.c
+++ src/executer/set_all_data_to_window.c
@@ -3,7 +3,7 @@
 char	set_all_data_to_window(t_cube cube)
 {
 	if (set_floor_and_ceil(cube))
-		return (EXIT_FAILURE);
+		return (EXIT_FAILURE);// print error !! (memory error)
 	if (handle_ray(cube))
 		return (EXIT_FAILURE);
 }
